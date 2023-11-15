d174bced8613a4595a519b5bf41dd19a8ab2023c
Haeo <eltsacbig63@gmail.com>
2023-11-15 16:47:37 +0900

    레벨업 이벤트 처리 수정(Slider 변경)
    기존 이벤트에서 애니메이션 효과를 위해 Image에서 Slider로 변경

--- Assets/Scripts/GameScene/LevelUpEvent.cs
+++ Assets/Scripts/GameScene/LevelUpEvent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,22 +9,26 @@ public class LevelUpEvent : MonoBehaviour
 {
     public TMP_Text expText;
     public TMP_Text levelText; 
-    public Image expBar;
 
-    private int expInterval = 50;   // 경험치 할당량
-    private int currentExp = 0; // 현재 경험치
-    private int maxExp = 100;   // 최대 경험치
-    private int currentlevel = 1;   // 현재 레벨
+    private float expInterval = 30f;   // 경험치 할당량
+    private float currentExp = 0; // 현재 경험치
+    private float maxExp = 100f;   // 최대 경험치
+    private float currentlevel = 1f;   // 현재 레벨
 
-    private int currentWidthBar = 0;    // 현재 경험치 바의 가로
-    private int currentHeightBar = 45;  // 경험치 바의 세로
-    private int maxWidthBar = 1000; // 최대 경험치 바 가로 길이
-  
+
+    public Slider expslider;   // 경험치 슬라이더
+
+    private float targetProgress = 0;   // 목표치
+    public float fillSpeed = 3.0f;  // 경험치 차는 속도
+
+    private void Awake()
+    {
+        expslider = GameObject.Find("ExpSlider").GetComponent<Slider>();
+    }
 
     private void Start()
     {
         UpdateExpText();
-        UpdateExpBarWidth();
     }
 
     void UpdateExpText()    // ExpText의 text 내용 변경
@@ -31,18 +36,13 @@ public class LevelUpEvent : MonoBehaviour
         expText.text = "Exp(" + currentExp + " / " + maxExp + ")";
     }
 
-    void UpdateExpBarWidth()
-    {
-        expBar.rectTransform.sizeDelta = new Vector2(currentWidthBar, currentHeightBar);
-    }
     
     void GainExp()    // 경험치 얻었을 경우
     {
         currentExp += expInterval;
-
-        currentWidthBar += expInterval * 10;
-        UpdateExpBarWidth();
         UpdateExpText();
+
+        targetProgress = currentExp * 0.01f;    // 경험치량을 정규화(0 ~ 1 사이 값)
 
         if (currentExp >= maxExp) // 레벨업
         {
@@ -51,8 +51,7 @@ public class LevelUpEvent : MonoBehaviour
             currentlevel++;
             levelText.text = "Lv. " + currentlevel;
 
-            currentWidthBar -= maxWidthBar;
-            UpdateExpBarWidth();
+            targetProgress = 1.0f;  // 우선, 100%로 경험치 할당 => Update 함수에서 다음 할당량 처리
         }
     }
 
@@ -60,8 +59,17 @@ public class LevelUpEvent : MonoBehaviour
     {
         if(Input.GetKeyUp(KeyCode.Space))   // 임시로 스페이스 바 누를 시 경험치 30 획득
         {
-            //GainExp(expInterval);
             GainExp();
+        }
+
+        if(expslider.value <= targetProgress)
+        {
+            expslider.value += fillSpeed * Time.deltaTime;
+            if (expslider.value == 1.0f)    // 슬라이더 값이 100% 채웠을 때
+            {
+                targetProgress = currentExp * 0.01f;    // 목표치 재할당
+                expslider.value = 0f;   // 슬라이더 값 초기화
+            }
         }
     }
 }
