4a3ddc613043a661a061c75e61a4262361be8e97
wooseokjang <jws314046@naver.com>
2023-11-22 13:32:01 +0900

    인공위성 추락, 지구 체력

---
+++ Assets/Scripts/GameScene/EarthHP.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EarthHP : MonoBehaviour
+{
+    [SerializeField] Slider hp_splider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp_splider.value = hp_splider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (hp_splider.value <= 0)
+        //{
+            
+        //}
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("EnemyLayer"))
+        {
+            hp_splider.value--;
+            Destroy(collision.gameObject);//인공위성 파괴
+
+        }
+    }
+}
--- Assets/Scripts/GameScene/EnemyRotateRound.cs
+++ Assets/Scripts/GameScene/EnemyRotateRound.cs
@@ -11,14 +11,47 @@ public class EnemyRotateRound : MonoBehaviour
 {
     public Transform center;    // 중심에 해당하는 물체
     private float rotation_speed;   // 회전 속도
+    private float timer;
+
+    private bool isRotating;
 
     private void Start()
     {
         rotation_speed = Random.Range(10.0f, 50.0f);    // 10 ~ 50 사이의 랜덤 값
+        timer = 0f;
+        isRotating = true;
     }
 
     private void Update()
     {
-        transform.RotateAround(center.position, Vector3.forward, rotation_speed * Time.deltaTime);  // 중심에서 원주 방향으로 회전 속도만큼 이동
+        if (isRotating)
+        {
+            transform.RotateAround(center.position, Vector3.forward, rotation_speed * Time.deltaTime);  // 중심에서 원주 방향으로 회전 속도만큼 이동
+            timer += Time.deltaTime;
+
+            if(timer >= 5f)
+            {
+                isRotating = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, center.position, Time.deltaTime * rotation_speed * 0.05f);
+
+            // Optionally, you can also rotate the enemy towards the center while moving
+            // Uncomment the line below if you want the enemy to rotate while moving
+            // transform.rotation = Quaternion.LookRotation(Vector3.forward, center.position - transform.position);
+        }
     }
+
+
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.layer == LayerMask.NameToLayer("Earth"))
+    //    {
+    //        Debug.Log("gd");
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
