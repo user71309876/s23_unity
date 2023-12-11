02f6a037e0858e1921e6782fdfd9601133ceae2e
Haeo <eltsacbig63@gmail.com>
2023-12-12 05:02:28 +0900

    속도 느려지는 타워 추가

---
+++ Assets/Scripts/GameScene/SlowDownTower.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using UnityEngine;
+
+public class SlowDownTower : MonoBehaviour
+{
+    //public LayerMask enemyLayer;
+    private float slowRadius = 3f;
+    public float slowFactor = 0.5f;
+
+    void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, slowRadius, LayerMask.GetMask("EnemyLayer"));
+
+        foreach (Collider collider in colliders)
+        {
+            EnemyRotateRound enemyRotate = collider.GetComponent<EnemyRotateRound>();
+
+            if (enemyRotate != null)
+            {
+                // 적의 속도를 느리게 만들기
+                enemyRotate.ApplySlow(slowFactor);
+            }
+        }
+
+        EnemyRotateRound[] allEnemies = GameObject.FindObjectsOfType<EnemyRotateRound>();
+
+        foreach (EnemyRotateRound enemy in allEnemies)
+        {
+            if (!colliders.Contains(enemy.GetComponent<Collider>()))
+            {
+                // Enemy is outside the slow radius
+                enemy.CancelSlow();
+            }
+        }
+    }
+}
